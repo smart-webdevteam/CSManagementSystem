93dc4cd0810cffa37b0c740aaaccdb3a2b5804a9
cheikh boudraa <cboudraa@genesis-consult.com>
2021-09-25 23:42:52 +0200

    Small changes

--- src/main/java/com/csms/CSManagementSystemApplication.java
+++ src/main/java/com/csms/CSManagementSystemApplication.java
@@ -10,7 +10,7 @@ public class CSManagementSystemApplication {
 
         SpringApplication.run(CSManagementSystemApplication.class, args);
 
-        System.out.println("Hello");
+        System.out.println("Hello Cheikh");
     }
 
 }
